c8f80129f2de16a1cac0b1160d9c46c18be46c19
Наталья <kadyshevani@gmail.ru>
2023-12-14 14:22:33 +0300

    Add my redirect request homework

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/hello")
-print (response.text)
+payload = {"name": "User"}
+response = requests.get("https://playground.learnqa.ru/api/hello", params = payload)
+print(response.text)
+parsed_response_text = response.json()
+print(parsed_response_text["answer"])
+
+response = requests.get("https://playground.learnqa.ru/api/get_301", allow_redirects = True)
+first_response = response.history[0]
+second_response = response
+print(response.status_code)
+print(first_response.url)
+print(second_response.url)
