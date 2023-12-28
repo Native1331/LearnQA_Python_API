ba3aa1f380649c8fa554cb2c686664d55411a33d
Наталья <kadyshevani@gmail.ru>
2023-12-28 21:13:42 +0300

    Add my tests with put request user  homework

--- hw/tests/test_user_get_hw.py
+++ hw/tests/test_user_get_hw.py
@@ -1,7 +1,3 @@
-import requests
-
-from datetime import datetime
-
 from lib.Assertions import Assertions
 from lib.base_case import BaseCase
 from lib.my_request import MyRequests
@@ -37,6 +33,7 @@ class TestUserGetHw(BaseCase):
         }
         response2 = MyRequests.post("/user/login", data=login_data)
         print(response2.text)
+        Assertions.assert_code_status(response2,200)
         auth_sid = self.get_cookie(response2, "auth_sid")
         token = self.get_header(response2, "x-csrf-token")
         user_id_from_auth = self.get_json_value(response2, "user_id")
@@ -48,4 +45,3 @@ class TestUserGetHw(BaseCase):
         expected_keys = ["password", "firstName", "lastName", "email"]
         Assertions.assert_json_has_key(response3, "username")
         Assertions.assert_json_has_not_keys(response3, "expected_keys")
-
--- hw/tests/test_user_put_requests.py
+++ hw/tests/test_user_put_requests.py
@@ -1,51 +1,75 @@
-import requests
-from datetime import datetime
 from lib.base_case import BaseCase
 from lib.Assertions import Assertions
-
-
+from lib.my_request import MyRequests
 class TestChangeUsers(BaseCase):
 
     def test_change_user_without_auth(self):
         # REGISTER
-        register_data = self.prepare_registration_data(prepa)
-        response1 = requests.post("https://playground.learnqa.ru/api/user/", data=register_data)
-        Assertions.assert_code_status(response1, 200)
-        Assertions.assert_json_has_key(response1, "id")
-        user_id = self.get_json_value(response1, 'id')
+        data = self.prepare_registration_data()
+        response = MyRequests.post("/user/", data=data)
+        Assertions.assert_code_status(response, 200)
+        Assertions.assert_json_has_key(response, "id")
+        user_id = self.get_json_value(response, 'id')
+
         # EDIT
         new_name = "Changed Name"
-        response3 = requests.put(f"https://playground.learnqa.ru/api/{user_id}",
-                                 data={"first_name": new_name})
-        Assertions.assert_code_status(response3, 200)
+        response1 = MyRequests.put(f"/user/{user_id}",
+                                    data = {"firstName": new_name})
+        Assertions.assert_code_status(response1, 400)
 
 
     def test_change_email_adress_in_authorization_user(self):
         # REGISTER
-        register_data = self.prepare_registration_data()
-        response1 = requests.post("https://playground.learnqa.ru/api/user/", data=register_data)
-        actual_email = response1.json()["email"]
-        print(actual_email)
-        Assertions.assert_code_status(response1, 200)
-        Assertions.assert_json_has_key(response1, "id")
+        data = self.prepare_registration_data()
+        response2 = MyRequests.post("/user/", data=data)
+        Assertions.assert_code_status(response2, 200)
+        Assertions.assert_json_has_key(response2, "id")
+        email = data.get("email")
+        password = data.get("password")
 
-        user_id = self.get_json_value(response1, 'id')
-
-        # LOGIN
+        #AUTH
         login_data = {
             'email': email,
             'password': password
         }
-        response2 = requests.post("https://playground.learnqa.ru/api/login", data=login_data)
+        response3 = MyRequests.post("/user/login", data=login_data)
+        Assertions.assert_code_status(response3,200)
+        print(response3.text)
+        auth_sid = self.get_cookie(response3, "auth_sid")
+        token = self.get_header(response3,"x-csrf-token")
+        user_id_from_auth = self.get_json_value(response3, "user_id")
 
+         # EDIT
+        new_email = "123example.ru"
+        response2 = MyRequests.put(f"/user/{user_id_from_auth}",
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid},
+                                   data={"email": new_email})
+        Assertions.assert_code_status(response2, 400)
+    def test_change_name_to_one_letter(self):
+        # REGISTER
+        data = self.prepare_registration_data()
+        response = MyRequests.post("/user/", data=data)
+        Assertions.assert_code_status(response, 200)
+        Assertions.assert_json_has_key(response, "id")
+        user_id = self.get_json_value(response, 'id')
+        email = data.get("email")
+        password = data.get("password")
+        # AUTH
+        login_data = {
+            'email': email,
+            'password': password
+        }
+        response1 = MyRequests.post("/user/login", data=login_data)
+        print(response1.text)
         auth_sid = self.get_cookie(response1, "auth_sid")
         token = self.get_header(response1, "x-csrf-token")
-
+        user_id_from_auth = self.get_json_value(response1, "user_id")
         # EDIT
-        new_email = "123example.ru"
-        response3 = requests.put(f"https://playground.learnqa.ru/api/{user_id}",
-                                 headers={"auth_sid": auth_sid},
-                                 cookies={"x-csrf-token": token},
-                                 data={"email": new_email}
-                                 )
-        Assertions.assert_code_status(response3, 200)
+        new_name = "V"
+        response2 = MyRequests.put(f"/user/{user_id}",
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid},
+                                   data={"firstName": new_name})
+
+        Assertions.assert_code_status(response2, 400)
