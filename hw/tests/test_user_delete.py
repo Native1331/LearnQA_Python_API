ed342d1de3be2ac3b519740bc2dc548c0f919358
Наталья <kadyshevani@gmail.ru>
2023-12-28 22:05:50 +0300

    Add my tests delete of users  homework

---
+++ hw/tests/test_user_delete.py
@@ -0,0 +1,57 @@
+from lib.base_case import BaseCase
+from lib.Assertions import Assertions
+from lib.my_request import MyRequests
+class TestUserDelete(BaseCase):
+    def test_delete_user_without_auth(self):
+        # AUTH
+        login_data = {
+          'email': 'vinkotov@example.com',
+          'password': '1234'
+        }
+        response2 = MyRequests.post("/user/login", data=login_data)
+        Assertions.assert_code_status(response2, 200)
+        print(response2.text)
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+        user_id_from_auth = self.get_json_value(response2, "user_id")
+
+        # DELETE
+        response3 = MyRequests.delete(f"/user/{user_id_from_auth}",
+                                      headers={"x-csrf-token": token},
+                                      cookies={"auth_sid": auth_sid}
+                                      )
+        Assertions.assert_code_status(response3, 400)
+    def test_user_delete(self):
+        # REGISTER
+        data = self.prepare_registration_data()
+        response = MyRequests.post("/user/", data=data)
+        Assertions.assert_code_status(response, 200)
+        Assertions.assert_json_has_key(response, "id")
+        email = data.get("email")
+        password = data.get("password")
+
+        # AUTH
+        login_data = {
+            'email': email,
+            'password': password
+        }
+        response2 = MyRequests.post("/user/login", data=login_data)
+        Assertions.assert_code_status(response2, 200)
+        print(response2.text)
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+        user_id_from_auth = self.get_json_value(response2, "user_id")
+
+        #DELETE
+        response3 = MyRequests.delete(f"/user/{user_id_from_auth}",
+                                      headers={"x-csrf-token": token},
+                                      cookies={"auth_sid": auth_sid}
+                                      )
+        Assertions.assert_code_status(response3,200)
+
+        #CHECK_USER_ID
+        response4 = MyRequests.get(f"user/{user_id_from_auth}",
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid})
+        Assertions.assert_code_status(response4, 404)
+
